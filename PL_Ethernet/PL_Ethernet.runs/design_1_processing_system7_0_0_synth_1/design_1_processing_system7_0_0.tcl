86edf1e254f816c4f7372a55812669d17d2b0db2
fumimaker <fumi01200924@gmail.com>
2020-10-16 11:13:14 +0900

    add

--- PL_Ethernet/PL_Ethernet.runs/design_1_processing_system7_0_0_synth_1/design_1_processing_system7_0_0.tcl
+++ PL_Ethernet/PL_Ethernet.runs/design_1_processing_system7_0_0_synth_1/design_1_processing_system7_0_0.tcl
@@ -17,7 +17,6 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_param chipscope.maxJobs 3
 set_param project.vivado.isBlockSynthRun true
 set_msg_config -msgmgr_mode ooc_run
 create_project -in_memory -part xc7z020clg400-1
@@ -28,6 +27,7 @@ set_param synth.vivado.isSynthRun true
 set_msg_config -source 4 -id {IP_Flow 19-2162} -severity warning -new_severity info
 set_property webtalk.parent_dir C:/Github/Zybo_mipi/PL_Ethernet/PL_Ethernet.cache/wt [current_project]
 set_property parent.project_path C:/Github/Zybo_mipi/PL_Ethernet/PL_Ethernet.xpr [current_project]
+set_property XPM_LIBRARIES {XPM_CDC XPM_MEMORY} [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
 set_property ip_output_repo c:/Github/Zybo_mipi/PL_Ethernet/PL_Ethernet.cache/ip [current_project]
