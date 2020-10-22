8d38826ddf7da3d702e31f49668eeace715f44af
fumi <fumi01200924@gmail.com>
2020-10-22 22:52:13 +0900

    原因判明、自分があほでした

--- Pcam2019/vitis/Pcam5C-app/src/ov5640/AXI_VDMA.h
+++ Pcam2019/vitis/Pcam5C-app/src/ov5640/AXI_VDMA.h
@@ -244,9 +244,11 @@ public:
 		int address = context_.WriteCfg.FrameStoreStartAddr[prevFrame];
 		char *pointer = (char *)address;//データ用
 		for(int i=0; i<1280; i++){
-			xil_printf("%p: %06X ",pointer, ((*pointer << 16) | *(pointer+1) << 8) | *(pointer+2) );
-			pointer += 3;
-			xil_printf("\n\r");
+			for(int j=0; j<720; j++){
+				xil_printf("%x : %p: %06X ",address, pointer, ((*pointer << 16) | *(pointer+1) << 8) | *(pointer+2) );
+							pointer += 3;
+							xil_printf("\n\r");
+			}
 		}
 		//xil_printf("\n\r");
 	}
